1e00e0b2be011dfb7572904101d374288e3efa9b
zhanyongzhi <yongzhi.zhan@gmail.com>
2017-03-02 16:26:04 +0800

    add solr

---
+++ solr/function_query.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import unittest
+
+'''
+
+'''
+
+class FunctionQueryTest(unittest.TestCase):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
