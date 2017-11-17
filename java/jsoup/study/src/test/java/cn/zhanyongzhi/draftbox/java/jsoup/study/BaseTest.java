c31525a2672901245319151ae39c53492a48673a
yongzhi.zhan <yongzhi.zhan@gmail.com>
2017-11-17 20:16:21 +0800

    增加代理和cookie测试

--- java/jsoup/study/src/test/java/cn/zhanyongzhi/draftbox/java/jsoup/study/BaseTest.java
+++ java/jsoup/study/src/test/java/cn/zhanyongzhi/draftbox/java/jsoup/study/BaseTest.java
@@ -1,6 +1,8 @@
 package cn.zhanyongzhi.draftbox.java.jsoup.study;
 
+import org.eclipse.jetty.proxy.ProxyServlet;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.jsoup.Connection;
@@ -12,15 +14,17 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.EnumSet;
 import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class BaseTest {
     @BeforeClass
@@ -67,6 +71,67 @@ public class BaseTest {
         assertEquals(body, "ok_bar");
     }
 
+    @Test
+    public void testUserAgent() throws Exception {
+        startServer();
+
+        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+
+        Connection connection = Jsoup.connect("http://localhost:9600/user-agent");
+        connection.userAgent(userAgent);
+        Connection.Request request = connection.request();
+        request.method(Connection.Method.POST);
+
+        //set session
+        Connection.Response response = connection.execute();
+        String body = response.body();
+
+        assertEquals(body, userAgent);
+    }
+
+    @Test
+    public void testProxy() throws Exception {
+        startServer();
+        startProxyServer();
+
+        Connection connection = Jsoup.connect("http://localhost:9600/test");
+        connection.proxy("127.0.0.1", 9601);
+
+        Connection.Request request = connection.request();
+        request.method(Connection.Method.POST);
+
+        String body = connection.execute().body();
+        assertTrue(body.startsWith("proxy server mark:"));
+    }
+
+    private void startProxyServer() throws Exception {
+        Server server = new Server(9601);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addFilter(new FilterHolder(new Filter() {
+            public void init(FilterConfig filterConfig) throws ServletException {
+
+            }
+
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                chain.doFilter(request, response);
+
+                response.getOutputStream().write("proxy server mark:".getBytes());
+            }
+
+            public void destroy() {
+
+            }
+        }), "/*", EnumSet.of(DispatcherType.REQUEST));
+
+        context.addServlet(new ServletHolder(new ProxyServlet()), "/*");
+
+        server.start();
+    }
+
     private void startServer() throws Exception {
         Server server = new Server(9600);
 
@@ -93,6 +158,12 @@ public class BaseTest {
                     Assert.assertTrue(1 == val);
 
                     response.getWriter().print("ok_bar");
+                } else if (path.equals("/user-agent")) {
+                    String userAgent = request.getHeader("User-Agent");
+                    response.getWriter().print(userAgent);
+                } else if (path.equals("/test")) {
+                    response.getWriter().print(String.format("test host:%s, port:%d",
+                            request.getRemoteHost(), request.getRemotePort()));
                 }
             }
         }), "/*");
